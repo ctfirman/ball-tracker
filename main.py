7f36e6961d7398ecbe6d08e75fd8f065300ed445
Chris Firmani <chrisfirmani@hotmail.com>
2020-12-18 16:52:00 -0500

    restructured class

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+'''
+Main file
+'''
 from src.BallTracker import BallTracker
 
-track = BallTracker("test_vids/Soccer.mp4")
-track.read_file()
+# For Soccer
+ball_lower = (20,200,100)
+ball_upper = (35,255,200)
+
+# For Ping
+# ball_lower = (30,0,0)
+# ball_upper = (40,255,255)
+
+track = BallTracker("test_vids/Soccer.mp4", ball_lower, ball_upper)
+track.read_vid()
--- src/BallTracker.py
+++ src/BallTracker.py
@@ -5,13 +5,55 @@ import imutils
 
 
 class BallTracker:
-    def __init__(self, file_name):
+    '''
+    None
+    '''
+    def __init__(self, file_name, lower_bound, upper_bound):
         self.file_name = file_name
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
 
-    def read_file(self):
+        self.points = deque(maxlen=15)
+
+    def __img_process(self, img):
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        mask = cv2.inRange(hsv, self.lower_bound, self.upper_bound) 
+
+        mask = cv2.dilate(mask, None)
+        mask = cv2.erode(mask, None)
+
+        return mask
+
+
+    def __draw_trail(self, frame, center):
+        self.points.appendleft(center)
+
+        for i in range(1, len(self.points)):
+            if self.points[i-1] is None or self.points[i] is None:
+                continue
+            # cv2.line(frame, points[i-1], points[i], (0,0,255), 2)
+            cv2.circle(frame, self.points[i], 4, (0,0,255), -1)
+
+
+    def __draw_circle(self, frame, contour,):
+        if len(contour) > 0:
+            c = max(contour, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            # Change int(20) to radius to get a changing circle
+            cv2.circle(frame, (int(x), int(y)), int(20), (0, 255, 255), 5)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            return center
+
+
+    def read_vid(self):
+        '''
+        Main Method to track the ball
+        '''
         cap = cv2.VideoCapture(self.file_name)
-
-        points = deque(maxlen=15)
 
         while cap.isOpened():
             _, frame = cap.read()
@@ -19,43 +61,18 @@ class BallTracker:
             if frame is None:
                 break
 
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            # For Soccer
-            ball_lower = (20,200,100)
-            ball_upper = (35,255,200)
-
-            # For Ping
-            # ball_lower = (30,0,0)
-            # ball_upper = (40,255,255)
-
-            mask = cv2.inRange(hsv, ball_lower, ball_upper) 
-
-            mask = cv2.dilate(mask, None)
-            mask = cv2.erode(mask, None)
+            mask = self.__img_process(frame)
 
             cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = imutils.grab_contours(cnts)
             center = None
 
-            if len(cnts) > 0:
-                c = max(cnts, key=cv2.contourArea)
-                ((x, y), radius) = cv2.minEnclosingCircle(c)
-                M = cv2.moments(c)
-                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            center = self.__draw_circle(frame, cnts)
 
-                # Change int(20) to radius to get a changing circle
-                cv2.circle(frame, (int(x), int(y)), int(20), (0, 255, 255), 5)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-            points.appendleft(center)
-            for i in range(1, len(points)):
-                if points[i-1] is None or points[i] is None:
-                    continue
-                cv2.line(frame, points[i-1], points[i], (0,0,255), 2)
+            self.__draw_trail(frame, center)
 
 
             cv2.imshow("frame", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    break
+                cv2.destroyAllWindows()
+                break
